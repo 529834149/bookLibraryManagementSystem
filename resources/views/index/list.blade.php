4ddac357a6c01d73f068da25600aa1b8a32859c9
529834149@qq.com <529834149@qq.com>
2019-07-31 14:34:11 +0800

    no message

--- resources/views/index/list.blade.php
+++ resources/views/index/list.blade.php
@@ -12,48 +12,54 @@
             <div class="layui-col-xs12 layui-col-sm12 layui-col-md8">
                 <div class="main">
                     <div class="list-item" id="LAY_demo2">
-                        @foreach($article_list as $v)
-                            @if($v->image)
-                                <div class="item">   
-                                    <a href="/article/{{$v->id}}">
-                                        <img width="160" height="100" lay-src="/public/uploads/{{$v->image}}" src="/public/uploads/{{$v->image}}">
-                                    </a>
-                                    <div class="item-info">
-                                        <h4><a href="article/{{$v->id}}">{{$v->article_title}}</a></h4>
-                                        <div class="b-txt">
-                                            <span class="label"><a style="color:#E6F8EC;" href="/category/{{$v->id}}/{{$v->short_name}}">{{$v->cate_title}}</a></span>
-                                            <span class="icon message">
-                                              <i class="layui-icon layui-icon-dialogue"></i>
-                                              {{$v->post_num}}条
-                                            </span>
-                                            <span class="icon time">
-                                              <i class="layui-icon layui-icon-log"></i>
-                                                 {{ \Carbon\Carbon::parse($v->created_at)->diffForHumans() }}
-                                            </span>
+                        @if(count($article_list)> 0)
+                            @foreach($article_list as $v)
+                                @if($v->image)
+                                    <div class="item">   
+                                        <a href="/article/{{$v->id}}">
+                                            <img width="160" height="100" lay-src="/public/uploads/{{$v->image}}" src="/public/uploads/{{$v->image}}">
+                                        </a>
+                                        <div class="item-info">
+                                            <h4><a href="article/{{$v->id}}">{{$v->article_title}}</a></h4>
+                                            <div class="b-txt">
+                                                <span class="label"><a style="color:#E6F8EC;" href="/category/{{$v->id}}/{{$v->short_name}}">{{$v->cate_title}}</a></span>
+                                                <span class="icon message">
+                                                  <i class="layui-icon layui-icon-dialogue"></i>
+                                                  {{$v->post_num}}条
+                                                </span>
+                                                <span class="icon time">
+                                                  <i class="layui-icon layui-icon-log"></i>
+                                                     {{ \Carbon\Carbon::parse($v->created_at)->diffForHumans() }}
+                                                </span>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            @else
-                                <div class="item">   
-                                    <div class="item-info">
-                                        <h4><a href="/article/{{$v->id}}">{{$v->article_title}}</a></h4>
-                                        <div class="b-txt">
-                                            <span class="label">{{$v->cate_title}}</span>
-                                            <span class="icon message">
-                                              <i class="layui-icon layui-icon-dialogue"></i>
-                                               {{$v->post_num}}条
-                                            </span>
-                                            <span class="icon time">
-                                              <i class="layui-icon layui-icon-log"></i>
-                                              {{ \Carbon\Carbon::parse($v->created_at)->diffForHumans() }}
+                                @else
+                                    <div class="item">   
+                                        <div class="item-info">
+                                            <h4><a href="/article/{{$v->id}}">{{$v->article_title}}</a></h4>
+                                            <div class="b-txt">
+                                                <span class="label">{{$v->cate_title}}</span>
+                                                <span class="icon message">
+                                                  <i class="layui-icon layui-icon-dialogue"></i>
+                                                   {{$v->post_num}}条
+                                                </span>
+                                                <span class="icon time">
+                                                  <i class="layui-icon layui-icon-log"></i>
+                                                  {{ \Carbon\Carbon::parse($v->created_at)->diffForHumans() }}
 
-                                            </span>
+                                                </span>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            @endif
-                        
-                        @endforeach
+                                @endif
+                            
+                            @endforeach
+                        @else
+                         <div class="item">   
+                              <p>暂无数据</p>
+                          </div>
+                        @endif
                        
                     </div>
                 </div>
