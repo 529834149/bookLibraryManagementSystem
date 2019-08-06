57736a3d2803b39e139446256cefa41c92f59129
529834149@qq.com <529834149@qq.com>
2019-08-06 19:31:36 +0800

    no message

---
+++ public/default/js/post_editor.js
@@ -0,0 +1,39 @@
+// ;(function($,window){
+//     var post_editor = {
+//         init:function(){
+//             //this.show_exam_status();
+// 			//is_show_status 状态是否存在
+// 			//this.validation();//表单验证
+// 			this.create_post();
+//         } ,
+//         create_post:function(){
+//             $('#create_post').click(function(){
+//                 layer.open({
+//                     closeBtn: 1,
+//                     scrollbar: false,// 屏蔽浏览器滚动
+//                     anim: 1,
+//                     shade: 0.5,
+//                     resize:false,
+//                     type: 2,
+//                     title: '发帖',
+//                     maxmin: false,
+//                     shadeClose: true, //点击遮罩关闭层
+//                     area : ['800px' , '800px'],
+//                     content: '/create_post',
+                   
+//                     cancel: function(index, layero){ 
+                      
+//                     },
+//                     success: function(layero){
+                        
+                        
+//                     },
+//                 });
+//             })
+//         }
+//     }
+//     window.post_editor = post_editor;
+//     $(function(){
+//         post_editor.init();
+//     });
+// }(jQuery,window));
--- resources/views/article/create_editor.blade.php
+++ resources/views/article/create_editor.blade.php
@@ -3,54 +3,77 @@
 
 
 @section('content')
-          
-<blockquote class="layui-elem-quote layui-text">
+<div class="layui-container" > 
+    <blockquote class="layui-elem-quote layui-text">
     朋友们有好的文章分享出来哦，
-</blockquote>
-<form class="layui-form" action="" lay-filter="example">
-    <div class="layui-form-item">
-        <label class="layui-form-label">文章分类</label>
+    </blockquote>
+    <form class="layui-form" action="" lay-filter="example" style="border:1px solid #E6E6E6">
+        <div class="layui-form-item">
+            <label class="layui-form-label">分类</label>
+            <div class="layui-input-block">
+              <select name="interest" lay-filter="aihao">
+                <option value=""></option>
+                <option value="0">写作</option>
+                <option value="1">阅读</option>
+                <option value="2">游戏</option>
+                <option value="3">音乐</option>
+                <option value="4">旅行</option>
+              </select>
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">标题</label>
+            <div class="layui-input-block">
+                <input type="text" name="username" lay-verify="title" autocomplete="off" placeholder="请输入标题" class="layui-input">
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">作者</label>
+            <div class="layui-input-block">
+                <input type="text" name="username" lay-verify="title" autocomplete="off" placeholder="请输入标题" class="layui-input">
+            </div>
+        </div>
+         <div class="layui-form-item">
+            <label class="layui-form-label">首图</label>
+            <div class="layui-input-block">
+               <button type="button" class="layui-btn" id="test1">
+                  <i class="layui-icon">&#xe67c;</i>上传图片
+                </button>
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">内容</label>
+            <div class="layui-input-block"> <textarea id="demo" style="display: none;"></textarea></div>
+           
+        </div>
+      <div class="layui-form-item">
         <div class="layui-input-block">
-          <select name="interest" lay-filter="aihao">
-            <option value=""></option>
-            <option value="0">写作</option>
-            <option value="1">阅读</option>
-            <option value="2">游戏</option>
-            <option value="3">音乐</option>
-            <option value="4">旅行</option>
-          </select>
+          <button class="layui-btn" lay-submit="" lay-filter="demo1">立即提交</button>
         </div>
-    </div>
-    <div class="layui-form-item">
-        <label class="layui-form-label">文章标题</label>
-        <div class="layui-input-block">
-            <input type="text" name="username" lay-verify="title" autocomplete="off" placeholder="请输入标题" class="layui-input">
-        </div>
-    </div>
-    <div class="layui-form-item">
-        <label class="layui-form-label">文章作者</label>
-        <div class="layui-input-inline">
-            <input type="text" name="username" lay-verify="title" autocomplete="off" placeholder="请输入标题" class="layui-input">
-        </div>
-    </div>
-    <div class="layui-form-item">
-        <label class="layui-form-label">文章内容</label>
-        <textarea id="demo" style="display: none;"></textarea>
-    </div>
-  <div class="layui-form-item">
-    <div class="layui-input-block">
-      <button class="layui-btn" lay-submit="" lay-filter="demo1">立即提交</button>
-    </div>
-  </div>
-</form>
+      </div>
+    </form>
+</div>          
+
 <script type="text/javascript" src="/default/res/layui/layui.js"></script>
 <!-- 注意：如果你直接复制所有代码到本地，上述js路径需要改成你本地的 -->
 <script>
-layui.use(['form', 'layedit', 'laydate'], function(){
+layui.use(['form', 'layedit', 'laydate','upload'], function(){
     var form = layui.form,
         layer = layui.layer,
         layedit = layui.layedit
         layedit.build('demo'); //建立编辑器
+        var upload = layui.upload;
+        //执行实例
+        var uploadInst = upload.render({
+            elem: '#test1' //绑定元素
+            ,url: '/upload/' //上传接口
+            ,done: function(res){
+              //上传完毕回调
+            }
+            ,error: function(){
+              //请求异常回调
+            }
+        });
   //监听提交
   form.on('submit(demo1)', function(data){
     layer.alert(JSON.stringify(data.field), {
